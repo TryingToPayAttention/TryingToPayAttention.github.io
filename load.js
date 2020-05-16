d811c858a60afc851487a60e9fd9912412a7e79f
AndrewMohebbi <amohebbi@ucla.edu>
2020-05-16 03:39:23 -0400

    automatic commit

--- load.js
+++ load.js
@@ -1,51 +1,36 @@
 
 const load = async () => {
     var posts = ["Post 1", "Post 2", "Post 3"]
-    rec(posts)
-}
-
-async function rec(posts) {
-    if (posts.length == 0) {
-        return
-    }
-
-    p = posts[0]
-
-    console.log("1we are here " + p)
+    const data = await get(posts)
+    for (i = 0; i < posts.length; i++){
+        p = posts[i]
 
         var post = document.createElement('div')
         post.className = "post"
         document.body.append(post)
 
-
-        console.log("3we are here " + p)
-
         var title = document.createElement('h2')
         title.innerHTML = p
         post.append(title)
-
-        console.log("5we are here " + p)
-
-        var url = '/posts/' + p
-        var data = await fetch(url).then(response => response.text())
-
-        console.log("7we are here " + p + " data: " + data)
 
         var par = document.createElement('p')
         par.innerHTML = "lol"
         post.append(par)
         
-
-        console.log("9we are here " + p)
-    
         var br = document.createElement('br')
         document.body.append(br)
-
-        console.log("10we are here " + p)
-
-        rec(posts.slice(1, posts.length -1))
-        return
+    }
 }
+
+const get = async (posts) => {
+    var ret = []
+    for (p of posts){
+        var url = '/posts/' + p
+        var data = await fetch(url).then(response => response.text())
+        ret.push(data)
+    }
+}
+
 
 
 load()
