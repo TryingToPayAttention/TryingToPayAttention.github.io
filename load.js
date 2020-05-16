bc41af9f6603f471606f7b4d8b07c329ac679013
AndrewMohebbi <amohebbi@ucla.edu>
2020-05-16 13:16:27 -0400

    automatic commit

--- load.js
+++ load.js
@@ -10,7 +10,7 @@ const load = () => {
             document.body.append(br)
 
             var post = document.createElement('div')
-            post.className = "post"
+            post.classList = ["post", "fade"]
             document.body.append(post)
 
             for (d of data[i].split(/\r?\n/)) { 
