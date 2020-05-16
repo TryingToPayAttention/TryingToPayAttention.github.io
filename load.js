c4db63c6f3e3faa9ef67139ebf66ee83f5d48b88
AndrewMohebbi <amohebbi@ucla.edu>
2020-05-16 11:24:27 -0400

    automatic commit

--- load.js
+++ load.js
@@ -11,16 +11,31 @@ const load = () => {
             document.body.append(post)
 
             for (d of data[i].split(/\r?\n/))   { 
+                // Title of post
                 if (d.slice(0, 10) === "##########"){
                     var el = document.createElement('h2')
                     el.innerHTML = d.slice(11)
                     post.append(el)
                 }
+                // Date published
                 else if (d.slice(0, 9) === "#########"){
                     var el = document.createElement('h6')
                     el.innerHTML = d.slice(10)
                     post.append(el)
                 }
+                // Header 3
+                else if (d.slice(0, 2) === "###"){
+                    var el = document.createElement('h3')
+                    el.innerHTML = d.slice(3)
+                    post.append(el)
+                }
+                // Header 4
+                else if (d.slice(0, 3) === "####"){
+                    var el = document.createElement('h4')
+                    el.innerHTML = d.slice(4)
+                    post.append(el)
+                }
+                // Paragraph
                 else{
                     var el = document.createElement('p')
                     el.innerHTML = d
