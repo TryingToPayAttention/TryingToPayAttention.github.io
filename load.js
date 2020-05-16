6e790253ba18d6a98838482fcca2425a5888f631
AndrewMohebbi <amohebbi@ucla.edu>
2020-05-16 03:09:58 -0400

    automatic commit

--- load.js
+++ load.js
@@ -19,7 +19,7 @@ const load = async () => {
         console.log("5we are here " + p)
 
         var url = '/posts/' + p
-        var data = await fetch(url).then(response => response.text()) 
+        var data = fetch(url).then(response => response.text()) 
         alert(data) 
 
         console.log("7we are here " + p)
