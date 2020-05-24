0fea587a91369c4317bf9fff40681e4605ceb1ed
AndrewMohebbi <amohebbi@ucla.edu>
2020-05-24 11:05:13 -0400

    automatic commit

---
+++ notes.js
@@ -0,0 +1,24 @@
+const load = () => {
+    //var modal = document.getElementById("myModal");
+    var btns = document.getElementsByClassName("superscript");
+
+    for (btn of btns) {
+        btn.onclick = function () {
+            var modal = document.getElementById("myModal1");
+            modal.style.display = "block";
+
+            var span = document.getElementsByClassName("close")[0];
+            span.onclick = function () {
+                modal.style.display = "none";
+            }
+
+            window.onclick = function (event) {
+                if (event.target == modal) {
+                    modal.style.display = "none";
+                }
+            }
+        }
+    }
+}
+
+load()
