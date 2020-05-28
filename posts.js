71ed6eed4baafcf41c5f07e197026350705b807b
AndrewMohebbi <amohebbi@ucla.edu>
2020-05-28 18:29:24 -0400

    automatic commit

--- posts.js
+++ posts.js
@@ -38,14 +38,14 @@ const load = () => {
             console.log("title clicked: " + event.target.innerHTML)
             var posts = document.getElementsByClassName("postContainer")
             var postList = document.getElementsByClassName("post")
-            //var saved
+            var saved
             for (p of postList){
               var t = p.getElementsByClassName("title")
               if (t[0].innerHTML != event.target.innerHTML){
                 p.style.display = "none" // Turn off posts that aren't of the clicked title
               } else {
                 p.className = "post" // Make sure this post is not faded
-                //saved = p
+                saved = p
               }
               posts[0].innerHTML = ""
               posts[0].append(saved)
